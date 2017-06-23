5599036ac31bb5d2714e868f37786289bfc2efa3
hupanda <u0706290@gmail.com>
2017-06-23 08:34:04 +0800

    remove mysql test in main
    remove mysql test in main

--- main.cpp
+++ main.cpp
@@ -10,13 +10,6 @@
 #include "ThostFtdcUserApiDataType.h"
 #include "ThostFtdcUserApiStruct.h"
 #include "CtpTraderSpi.h"
-#include "mysql_connection.h"
-#include <cppconn/driver.h>
-#include <cppconn/exception.h>
-#include <cppconn/resultset.h>
-#include <cppconn/statement.h>
-#include <cppconn/prepared_statement.h>
-using namespace std;
 
 int main()
 {
@@ -31,27 +24,6 @@ int main()
                
   try
   {
-	  sql::Driver *driver;
-	  sql::Connection *con;
-	  sql::PreparedStatement *pstmt;
-	  driver = get_driver_instance();
-	  con = driver->connect("tcp://127.0.0.1:3306", "admin", "admin");
-	  con->setSchema("ctp");
-	  string statement = "INSERT INTO trades (OrderSysID, ExchangeInstID, OffsetFlag, Side, Price, Size, TradeTime, TradeDate, TradeID) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-	  pstmt = con->prepareStatement(statement);
-	  pstmt->setInt(1, 1);
-	  pstmt->setString(2, "a");
-	  pstmt->setString(3, "a");
-	  pstmt->setString(4, "a");
-	  pstmt->setDouble(5, 1.1);
-	  pstmt->setInt(6, 1);
-	  pstmt->setDateTime(7, "2017-06-22 22:22:22");
-	  pstmt->setDateTime(8, "2017-06-22");
-	  pstmt->setInt(9, 3);
-	  pstmt->execute();
-	  delete pstmt;
-	  delete con;
-
 	  const std::string tdflowpathstring = "C:/Users/hupanda/CTP/tdflow";
 	  const char * tdflowpath = tdflowpathstring.c_str();
 	  CThostFtdcTraderApi* api = CThostFtdcTraderApi::CreateFtdcTraderApi(tdflowpath);
